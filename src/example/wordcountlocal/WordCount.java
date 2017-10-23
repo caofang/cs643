4d268a5c5d616e8dd32c1325087c8d0f690947ce
FANG CAO <fc63@njit.edu>
2017-10-23 17:22:12 -0400

    ignor case

---
+++ src/example/wordcountlocal/WordCount.java
@@ -0,0 +1,30 @@
+
+import java.io.IOException;
+import java.util.regex.Pattern;
+import java.util.regex.Matcher;
+
+
+
+public class WordCount  {
+
+    public static void main(String[] args) throws Exception {
+
+        System.out.print("===== trace =====\n");
+
+        
+    
+        String in = "hello hello world\n\n hello world hello ";
+        int i = 0;
+        Pattern p = Pattern.compile("hello");
+        Matcher m = p.matcher( in );
+        while (m.find()) {
+            i++;
+        }
+        System.out.println(i); // Prints 2
+                
+
+
+
+
+    }
+}
